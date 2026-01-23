7a55136590ab676a978045f89f594d0001cd8ec4
Miyamura1101 <felipesilsou1101@gmail.com>
2026-01-23 00:19:53 -0300

    Criação do Modelo referente a Organização, realizando algumas alteração de tipo de variaveis para dar mais sentido no contexto

---
+++ Codigo/EvenPaceWeb/EvenPace/Models/OrganizacaoModel.cs
@@ -0,0 +1,60 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EvenPace.Models;
+
+public class OrganizacaoModel
+{
+    [Display(Name = "Código")]
+    [Key]
+    [Required(ErrorMessage = "Código da Organização é obrigatorio")]
+    public int Id { get; set; }
+    
+    [Display(Name = "CNPJ")]
+    [Required(ErrorMessage = "Campo Requirido")]
+    [StringLength(14, ErrorMessage = "O CNPJ deve conter 14 número")]
+    public string Cnpj { get; set; } = null!; // TODO: Consertar o Banco 
+    
+    [Display(Name = "CPF")]
+    [Required(ErrorMessage = "Campo Requirido")]
+    [StringLength(11, ErrorMessage = "O CNPJ deve conter 14 número")]
+    public string? Cpf { get; set; }         // TODO: Consertar o Banco 
+
+    [Required(ErrorMessage = "Telefone é Obrigatorio")]
+    [Phone(ErrorMessage = "Telefone invalido")]
+    [DataType(DataType.PhoneNumber)]
+    public string Telefone { get; set; } = null!;
+    
+    [Display(Name = "CEP")]
+    [Required(ErrorMessage = "O CEP é Obrigatorio")]
+    [StringLength(8, ErrorMessage = "O CEP deve conter 8 números")]
+    public string Cep { get; set; } = null!;
+
+    [Required]
+    public string Rua { get; set; } = null!;
+
+    [Required]
+    public string Bairro { get; set; } = null!;
+
+    [Required]
+    public string Cidade { get; set; } = null!;
+
+    [Required]
+    public int Numero { get; set; }
+
+    [Required]
+    public string Estado { get; set; } = null!;
+
+    [Required(ErrorMessage = "Email é obrigatorio")]
+    [EmailAddress(ErrorMessage = "Email invalido")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "Senha é Obrigatoria")]
+    [MinLength(6, ErrorMessage = "A senha deve pelo menos conter 6 caracteres")]
+    [DataType(DataType.Password)]
+    public string Senha { get; set; } = null!;
+    
+    public bool StatusSituacao { get; set; }
+
+    public int AdmistradorId { get; set; }
+}
