20334d811e7c251cd09da56797562b4b2b88cddb
Hevellyn De Souza <156838385+heveeedesouza@users.noreply.github.com>
2026-01-24 23:25:12 -0300

    Add CartaoCreditoProfile for mapping Cartaocredito

--- Codigo/EvenPaceWeb/EvenPace/Mappers/CartaoCredito.cs
+++ Codigo/EvenPaceWeb/EvenPace/Mappers/CartaoCredito.cs
@@ -1,1 +1,12 @@
+namespace EvenPaceWeb.Mappers
+{
+    public class CartaoCreditoProfile : Profile
+    {
+        public CartaoCreditoProfile()
+        {
+            CreateMap<Cartaocredito, CartaoCreditoViewModel>()
+                .ReverseMap();
+        }
+    }
+}
 
