137be50fac3fc6d06b596a6f6d248ad1d8d8aa9a
Hevellyn De Souza <156838385+heveeedesouza@users.noreply.github.com>
2026-01-24 23:42:25 -0300

    from CartaoCreditoService methods
    Removed XML documentation comments for methods in CartaoCreditoService.

--- Codigo/EvenPaceWeb/Service/CartaoCreditoService.cs
+++ Codigo/EvenPaceWeb/Service/CartaoCreditoService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Core;
 using Core.Service;
+
 namespace Service
 {
     internal class CartaoCreditoService : ICartaoCredito
@@ -15,23 +16,12 @@ namespace Service
         {
             _context = context;
         }
-
-        /// <summary>
-        /// Insere um cartão de crédito no banco de dados
-        /// </summary>
-        /// <param name="cartaoCredito"></param>
-        /// <returns>Retorna o Id do cartão</returns>
         public uint Insert(CartaoCredito cartaoCredito)
         {
             _context.Add(cartaoCredito);
             _context.SaveChanges();
             return cartaoCredito.Id;
         }
-
-        /// <summary>
-        /// Edita um cartão de crédito no banco de dados
-        /// </summary>
-        /// <param name="cartaoCredito"></param>
         public void Edit(CartaoCredito cartaoCredito)
         {
             if (cartaoCredito is not null)
@@ -42,10 +32,6 @@ namespace Service
             }
         }
 
-        /// <summary>
-        /// Deleta um cartão de crédito do banco de dados
-        /// </summary>
-        /// <param name="id"></param>
         public void Delete(int id)
         {
             var cartao = _context.CartaoCreditos.Find(id);
@@ -57,20 +43,11 @@ namespace Service
             }
         }
 
-        /// <summary>
-        /// Pega um cartão de crédito do banco de dados pelo Id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>Retorna o cartão</returns>
         public CartaoCredito Get(int id)
         {
             return _context.CartaoCreditos.Find(id);
         }
 
-        /// <summary>
-        /// Pega todos os cartões de crédito do banco de dados
-        /// </summary>
-        /// <returns>Retorna todos os cartões</returns>
         public IEnumerable<CartaoCredito> GetAll()
         {
             return _context.CartaoCreditos.ToList();
