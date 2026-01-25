dcdfeb02283b05c62afed2ab023f5cd980a2ae5b
Hevellyn De Souza <156838385+heveeedesouza@users.noreply.github.com>
2026-01-24 23:34:49 -0300

    Define ICartaoCredito interface with CRUD methods

--- Codigo/EvenPaceWeb/Core/Service/ICartaoCreditoService.cs
+++ Codigo/EvenPaceWeb/Core/Service/ICartaoCreditoService.cs
@@ -1,1 +1,14 @@
+using System.Collections.Generic;
+
+namespace Core.Service
+{
+    public interface ICartaoCredito
+    {
+        void Edit(CartaoCredito cartaoCredito);
+        uint Insert(CartaoCredito cartaoCredito);
+        CartaoCredito Get(int id);
+        void Delete(int id);
+        IEnumerable<CartaoCredito> GetAll();
+    }
+}
 
