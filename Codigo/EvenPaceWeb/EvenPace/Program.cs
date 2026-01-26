36e2d93106accbedef091e4b7475368440d88e93
Miyamura1101 <felipesilsou1101@gmail.com>
2026-01-26 20:13:01 -0300

    Pequena alteração no programa e no startup

--- Codigo/EvenPaceWeb/EvenPace/Program.cs
+++ Codigo/EvenPaceWeb/EvenPace/Program.cs
@@ -1,46 +1,20 @@
-using Core;
-using Core.Service;
-using Microsoft.EntityFrameworkCore;
-using Service;
-using EvenPace.Mappers; // Adicionei para garantir que ache o AutoMapper
-
 var builder = WebApplication.CreateBuilder(args);
 
-// --- 1. CONFIGURAÇÃO DOS SERVIÇOS (Trazido do Startup.cs) ---
-
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Configuração do Banco de Dados (MySQL)
-// Verifica se a string de conexão existe para evitar o aviso amarelo CS8604
-var connectionString = builder.Configuration.GetConnectionString("EvenPaceDatabase");
-builder.Services.AddDbContext<EvenPaceContext>(options =>
-    options.UseMySQL(connectionString ?? ""));
-
-// Registro dos Serviços (Injeção de Dependência)
-builder.Services.AddTransient<IAdministradorService, AdministradorService>();
-builder.Services.AddTransient<IAvaliacaoEventoService, AvaliacaoEventoService>();
-builder.Services.AddTransient<ICartaoCreditoService, CartaoCreditoService>();
-builder.Services.AddTransient<IEventosService, EventoService>();
-builder.Services.AddTransient<ICorredorService, CorredorService>();
-builder.Services.AddTransient<IInscricaoService, InscricaoService>();
-builder.Services.AddTransient<ICupomService, CupomService>();
-
-// A LINHA MÁGICA QUE FALTAVA:
-builder.Services.AddTransient<IKitService, KitService>();
-
-builder.Services.AddTransient<IOrganizacaoService, OrganizacaoService>();
-
-// Configuração do AutoMapper
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-// --- FIM DAS CONFIGURAÇÕES ---
+var startup = new EvenPaceWeb.Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
+
+startup.Configure(app, app.Environment);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -53,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Corredor}/{action=Index}/{id?}");
 
 app.Run();
--- Codigo/EvenPaceWeb/EvenPace/Startup.cs
+++ Codigo/EvenPaceWeb/EvenPace/Startup.cs
@@ -18,6 +18,7 @@ namespace EvenPaceWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             // Adicione outros serviços necessários aqui
             services.AddDbContext<EvenPaceContext>(options =>
                 options.UseMySQL(Configuration.GetConnectionString("EvenPaceDatabase")));
