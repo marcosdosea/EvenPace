69dff938e26ae9334eda76587535ae0d01f03e6f
Miyamura1101 <felipesilsou1101@gmail.com>
2026-01-25 22:37:48 -0300

    Mudança dos namespaces dos controlers, para que ficassem todos iguais

--- Codigo/EvenPaceWeb/EvenPace/Models/AvaliacaoEventoViewModel.cs
+++ Codigo/EvenPaceWeb/EvenPace/Models/AvaliacaoEventoViewModel.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace EvenPaceWeb.Models
+namespace Models
 {
     public class AvaliacaoEventoViewModel
     {
--- Codigo/EvenPaceWeb/EvenPace/Models/EventoViewModel.cs
+++ Codigo/EvenPaceWeb/EvenPace/Models/EventoViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace EvenPace.ViewModels
+namespace Models
 {
     public class EventoViewModel
     {
