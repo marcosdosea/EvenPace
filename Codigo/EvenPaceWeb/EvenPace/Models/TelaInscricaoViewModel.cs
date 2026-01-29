3aedfa7d5351e737f2bcc2d94791a99b353c338d
Hevellyn De Souza <156838385+heveeedesouza@users.noreply.github.com>
2026-01-28 23:42:11 -0300

    Update TelaInscricaoViewModel.cs

--- Codigo/EvenPaceWeb/EvenPace/Models/TelaInscricaoViewModel.cs
+++ Codigo/EvenPaceWeb/EvenPace/Models/TelaInscricaoViewModel.cs
@@ -1,27 +1,33 @@
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Models;
 
 namespace EvenPaceWeb.Models
 {
+    public class TelaInscricaoViewModel
+    {
+        public uint IdEvento { get; set; }
 
-        public class TelaInscricaoViewModel
-        {
-        
-          public uint IdEvento { get; set; }
-           public int? IdKit { get; set; }
-           public string NomeEvento { get; set; }
-            public string ImagemEvento { get; set; }
-            public string Local { get; set; }
-            public DateTime DataEvento { get; set; }
-            public string Descricao { get; set; }
+        public int? IdKit { get; set; }
+
+        public string NomeEvento { get; set; }
+
+        public string ImagemEvento { get; set; }
+
+        public string Local { get; set; }
+
+        public DateTime DataEvento { get; set; }
+
+        public string Descricao { get; set; }
 
         [Display(Name = "Informações de Retirada do Kit")]
         public string InfoRetiradaKit { get; set; } = null!;
+
         public List<string> Percursos { get; set; }
-            public List<KitViewModel> Kits { get; set; }
 
-            
-            public InscricaoViewModel Inscricao { get; set; }
-        }
+        public List<KitViewModel> Kits { get; set; }
+
+        public InscricaoViewModel Inscricao { get; set; }
     }
-
+}
