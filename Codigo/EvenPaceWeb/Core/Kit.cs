ecab76dfaeaf113497d79c6e3cc399b22377165f
Hevellyn De Souza <156838385+heveeedesouza@users.noreply.github.com>
2026-02-12 23:26:36 -0300

    Correção dos campos que estavam com nomes diferentes do banco

--- Codigo/EvenPaceWeb/Core/Kit.cs
+++ Codigo/EvenPaceWeb/Core/Kit.cs
@@ -3,33 +3,49 @@ using Core;
 
 public partial class Kit
 {
+    [Column("id")]
     public int Id { get; set; }
 
+    [Column("valor")]
     public decimal Valor { get; set; }
 
+    [Column("nome")]
     public string Nome { get; set; } = null!;
 
+    [Column("descricao")]
     public string Descricao { get; set; } = null!;
 
+    [Column("disponibilidadep")]
     public int DisponibilidadeP { get; set; }
+
+    [Column("disponibilidadeg")]
     public int DisponibilidadeG { get; set; }
+
+    [Column("disponibilidadem")]
     public int DisponibilidadeM { get; set; }
 
+    [Column("utilizadap")]
     public sbyte UtilizadaP { get; set; }
+
+    [Column("utilizadag")]
     public sbyte UtilizadaG { get; set; }
+
+    [Column("utilizadam")]
     public sbyte UtilizadaM { get; set; }
 
-    [Column("idEvento")]
+    [Column("idevento")]
     public int IdEvento { get; set; }
 
+    [Column("statusretiradakit")]
     public bool StatusRetiradaKit { get; set; }
 
+    [Column("dataretirada")]
     public DateTime DataRetirada { get; set; }
 
+    [Column("imagem")]
     public string? Imagem { get; set; }
 
     public virtual Evento IdEventoNavigation { get; set; } = null!;
 
-    public virtual ICollection<Inscricao> Inscricaos { get; set; } = new List<Inscricao>();
+    public virtual ICollection<Inscricao> Inscricao { get; set; } = new List<Inscricao>();
 }
-
