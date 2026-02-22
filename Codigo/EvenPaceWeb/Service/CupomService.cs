765adbad30e73d6ce2181bb4dfcbe65ac03d9639
Lorena Pereira Nascimento <lorena.lindaflower@gmail.com>
2026-02-22 20:11:47 -0300

    Ajuste Cupom service #221

--- Codigo/EvenPaceWeb/Service/CupomService.cs
+++ Codigo/EvenPaceWeb/Service/CupomService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core;
+﻿using Core;
 using Core.Service;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,11 +13,6 @@ namespace Service
             _context = context;
         }
 
-        /// <summary>
-        /// Insere um cupom no banco de dados
-        /// </summary>
-        /// <param name="cupom"></param>
-        /// <returns>Retorna o Id do cupom</returns>
         public int Create(Cupom cupom)
         {
             cupom.Id = 0;
@@ -31,20 +21,12 @@ namespace Service
             return cupom.Id;
         }
 
-        /// <summary>
-        /// Edita um cupom no banco de dados
-        /// </summary>
-        /// <param name="cupom"></param>
         public void Edit(Cupom cupom)
         {
             _context.Update(cupom);
             _context.SaveChanges();
         }
 
-        /// <summary>
-        /// Deleta um cupom do banco de dados
-        /// </summary>
-        /// <param name="id"></param>
         public void Delete(int id)
         {
             var cupom = _context.Cupoms.Find(id);
@@ -53,36 +35,20 @@ namespace Service
             _context.SaveChanges();
         }
 
-        /// <summary>
-        /// Pega um cupom do banco de dados pelo Id
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns>Retorna o cupom</returns>
         public Cupom Get(int id)
         {
-            //return _context.Cupoms.Find(id);
             return _context.Cupoms.FirstOrDefault(cupom => cupom.Id == id)
             ?? throw new ServiceException("Cupom não encontrado");
         }
 
-        /// <summary>
-        /// Pega todos os cupons do banco de dados
-        /// </summary>
-        /// <returns>Retorna todos os cupons no banco de dados</returns>
         public IEnumerable<Cupom> GetAll()
         {
             return _context.Cupoms.AsNoTracking();
         }
 
-        /// <summary>
-        /// Pega cupons pelo nome
-        /// </summary>
-        /// <param name="nome"></param>
-        /// <returns>Retorna todos os cupons com o nome inserido</returns>
         public IEnumerable<Cupom> GetByName(string nome)
         {
             return _context.Cupoms.Where(c => c.Nome.Contains(nome)).AsNoTracking();
         }
     }
-
 }
