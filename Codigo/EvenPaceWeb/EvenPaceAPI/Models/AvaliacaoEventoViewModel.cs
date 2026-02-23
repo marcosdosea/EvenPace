489ea85cce4c87cbf2abac1b37362c3ac3c73edc
Alanna Rayssa <156382230+alannarayssaa@users.noreply.github.com>
2026-02-23 18:13:37 -0300

    Refactor AvaliacaoEventoViewModel properties

--- Codigo/EvenPaceWeb/EvenPaceAPI/Models/AvaliacaoEventoViewModel.cs
+++ Codigo/EvenPaceWeb/EvenPaceAPI/Models/AvaliacaoEventoViewModel.cs
@@ -8,14 +8,11 @@ namespace Models
         public int Id { get; set; }
 
         [Required]
-        public int EventoId { get; set; }
+        public int InscricaoId { get; set; }
 
         [Required]
-        public int CorredorId { get; set; }
-
-        [Required(ErrorMessage = "A avaliação é obrigatória.")]
-        [Range(1, 5, ErrorMessage = "A avaliação deve ser entre 1 e 5.")]
-        public int Nota { get; set; }
+        [Range(1, 5, ErrorMessage = "A nota deve ser entre 1 e 5.")]
+        public int Estrela { get; set; }
 
         [StringLength(300)]
         public string? Comentario { get; set; }
