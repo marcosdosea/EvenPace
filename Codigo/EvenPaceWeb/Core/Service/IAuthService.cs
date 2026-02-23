ba4765fc80c2e901776fe8031e418cae4cdbc951
Lorena Pereira Nascimento <lorena.lindaflower@gmail.com>
2026-02-23 15:44:46 -0300

    Criação da IAuthService #224

---
+++ Codigo/EvenPaceWeb/Core/Service/IAuthService.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+
+namespace Core.Service
+{
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Realiza o login do usuário com base no e-mail e senha informados.
+        /// </summary>
+        Task<bool> LoginAsync(string email, string senha);
+
+        /// <summary>
+        /// Realiza o encerramento da sessão do usuário atual.
+        /// </summary>
+        Task LogoutAsync();
+
+        /// <summary>
+        /// Obtém o e-mail do usuário autenticado a partir dos claims contextuais.
+        /// </summary>
+        Task<string> GetUsuarioLogadoEmailAsync(ClaimsPrincipal user);
+    }
+}
